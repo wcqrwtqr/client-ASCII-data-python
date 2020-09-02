7a5a5c9f665d04499e017740fd33ec40ce2ffc40
Mohammed Albatati <mohammedalbatati@Mohammeds-MacBook-Pro.local>
2020-09-02 22:28:16 +0300

    re-arrange the codes inside the reading SWT ASCII.py

--- Reading SWT ASCII.py
+++ Reading SWT ASCII.py
@@ -10,13 +10,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #from bokeh.plotting import figure, output_file,show
 
+
+
+dfM = pd.read_csv('Merged2.csv',header=None,skiprows = 1, index_col = False,names=['Date','Time','Combo_Date','WHP','WHT','DSCP','CSGP','Oil line Temp','Static Press','Gas line Temp','Differential Press','Gas Rate (Inst) Ave','Gas Rate (average) Ave','Gas Cum','3" STOil Rate','3" STOil Cum','2" STOil Rate','2" STOil Cum'])
+
+dfM['Combo_Date'] = pd.to_datetime(dfM['Combo_Date'])
+
+
+
+
+plt.figure()
+plt.plot(dfM['Combo_Date'],dfM['2" STOil Rate'],'b-')
+plt.plot(dfM['Combo_Date'],dfM['3" STOil Rate'],'g-')
+plt.plot(dfM['Combo_Date'],dfM['WHP'],'r-')
+#plt.plot(x,MPFM_data.loc[n:m,'Temperature'],'r-')
+#plt.xticks(rotation='vertical')
+plt.xticks(rotation=45)
+# Pad margins so that markers don't get clipped by the axes
+plt.margins(0.05)
+plt.subplots_adjust(bottom=0.15)
+plt.legend()
+plt.show
+
+
+
+
+
+
+# Old data before the merged excel sheet is used:
+# Below code are in the event 
+
+
 #df2 = pd.read_csv('2 inch oil.csv',header=None,skiprows = 12,index_col = False,names=['Date','Time','empty','Oil line Temp','2" Measured Rate','2" Liquid Rate Ave','2" Liquid Cum','BS&W Cut','Oil SG','Oil SG Temp','API @60F','CMF','ST Temp','CTL','2" STOil Rate','2" STOil Cum','Water Rate','Water Cum'])
 #df3 = pd.read_csv('3 inch oil.csv',header=None,skiprows = 12, index_col = False,names=['Date','Time','empty','Meter Temp','3" Measured Rate','3" Liquid Rate','3" Liquid Cum','BS&W Cut','Oil SG','Oil SG Temp','API @60F','CMF','ST Temp','CTL','3" STOil Rate','3" STOil Cum','Water Rate','Water Cum'])
 #dfG = pd.read_csv('Gas.csv',header=None,skiprows = 12, index_col = False,names=['Date','Time','empty','Static Press','Gas line Temp','Differential Press','Plate Size','Gas G','Fn','Fc(Flange) / Fb(Pipe)','Fsl(Flange) / Fr(Pipe)','Fpv','Ftf','Ftb','Y','Fgr','Fpb','C','Gas Rate (Inst) Ave','Gas Rate (average) Ave','Gas Cum'])
 #dfW = pd.read_csv('WHP.csv',header=None,skiprows = 12, index_col = False,names=['Date','Time','empty','WHP','WHT','DSCP','CSGP','Oil line Temp','3" Measured Rate','3" Liquid Cum','3" STOil Rate','3" STOil Cum','2" Measured Rate','2" Liquid Cum','Measured Rate CPF','Water Cum'])
 
 
-dfM = pd.read_csv('Merged2.csv',header=None,skiprows = 1, index_col = False,names=['Date','Time','Combo_Date','WHP','WHT','DSCP','CSGP','Oil line Temp','Static Press','Gas line Temp','Differential Press','Gas Rate (Inst) Ave','Gas Rate (average) Ave','Gas Cum','3" STOil Rate','3" STOil Cum','2" STOil Rate','2" STOil Cum'])
+
 
 #Dropping===================================
 #df2['D_T'] = pd.to_datetime(df2['Date'] +' '+ df2['Time'])
@@ -53,7 +84,8 @@ dfM = pd.read_csv('Merged2.csv',header=None,skiprows = 1, index_col = False,name
 #df3 = df3.drop(['NN_x','NN_y','Storage_T_P_02_x','Gas Rate (average)_y','Gas Cum_y','SEP Press_y','Differential Press_y','Gas T_y','Date_y','Storage_T_P_02_y'],axis=1)
 
 #dfM['D_T'] = pd.to_datetime(dfM['Date'] +' '+ dfM['Time'])
-dfM['Combo_Date'] = pd.to_datetime(dfM['Combo_Date'])
+
+
 
 #print(df3.columns)
 #print(df3['Gas Rate (average)_x'])
@@ -63,18 +95,7 @@ dfM['Combo_Date'] = pd.to_datetime(dfM['Combo_Date'])
 
 
 
-plt.figure()
-plt.plot(dfM['Combo_Date'],dfM['2" STOil Rate'],'b-')
-plt.plot(dfM['Combo_Date'],dfM['3" STOil Rate'],'g-')
-plt.plot(dfM['Combo_Date'],dfM['WHP'],'r-')
-#plt.plot(x,MPFM_data.loc[n:m,'Temperature'],'r-')
-#plt.xticks(rotation='vertical')
-plt.xticks(rotation=45)
-# Pad margins so that markers don't get clipped by the axes
-plt.margins(0.05)
-plt.subplots_adjust(bottom=0.15)
-plt.legend()
-plt.show
+
 
 #plt.figure()
 #plt.plot(dfM['Combo_Date'],dfM['Gas Rate (average) Ave'],'b-')
